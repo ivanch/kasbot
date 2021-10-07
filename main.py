86dd08778611a5ea47690ca2e26229bb18afa0a9
José Henrique <jose.henrique.ivan@gmail.com>
2021-10-07 12:57:38 -0700

    suffix changes for dev environment

--- main.py
+++ main.py
@@ -5,7 +5,14 @@ from discord.ext import commands
 from player import Music
 from kasino import Kasino
 
-client = commands.Bot(command_prefix=['-', ''], description='Yet another music client.')
+from dotenv import load_dotenv
+load_dotenv()
+
+# Setup suffixes, set env suffixes if they're set, otherwise default
+MUSIC_SUFFIX = os.getenv("MUSIC_SUFFIX") or "-"
+KASINO_SUFFIX = os.getenv('KASINO_SUFFIX') or ""
+
+client = commands.Bot(command_prefix=[MUSIC_SUFFIX, KASINO_SUFFIX], description='Kasino DEEJAY.')
 music = Music(client)
 kasino = Kasino(client)
 client.add_cog(music)
@@ -13,31 +20,25 @@ client.add_cog(kasino)
 
 kasino_commands = []
 for cmd in kasino.get_commands():
-	kasino_commands.append(cmd.name)
+	kasino_commands.append(KASINO_SUFFIX + cmd.name)
 
 @client.event
 async def on_ready():
 	print('Logged in as {0.user}!'.format(client))
 
-	# Canal do KASINO
-	# await client.get_channel(887381114124136461).connect()
-
 @client.event
 async def on_message(message: discord.Message):
 	# Verifica se é um comando ou não
-	if message.content in kasino_commands or message.content.startswith('-'):
+	if message.content in kasino_commands or message.content.startswith(MUSIC_SUFFIX):
 		await client.process_commands(message)
 
 # #
-# Inicializar o bot
+# Init KASINÂO
 # #
 
-try:
-	alive = os.environ['alive']
-	if alive is not None:
-		from utils.keepalive import keep_alive
-		keep_alive()
-except:
-	pass
+alive = os.getenv('alive')
+if alive is not None:
+	from utils.keepalive import keep_alive
+	keep_alive()
 
 client.run(os.getenv("TOKEN"))
